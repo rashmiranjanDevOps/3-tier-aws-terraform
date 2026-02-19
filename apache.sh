8dcdb27f417f272039958196648d1fc7282b7a38
Rashmi ranjan panigrahy <rashmiranjan.devops@gmail.com>
2026-02-19 20:37:47 +0530

    Enhance apache.sh for deployment process
    Updated script to include system update and enable httpd service.

--- apache.sh
+++ apache.sh
@@ -1,7 +1,14 @@
-#! /bin/bash
-yum install httpd git -y
+#!/bin/bash
+yum update -y
+yum install -y httpd git
+
 systemctl start httpd
-systemctl status httpd
+systemctl enable httpd
+
 cd /var/www/html
+rm -rf *
 git clone https://github.com/karishma1521success/swiggy-clone.git
 mv swiggy-clone/* .
+chown -R apache:apache /var/www/html
+
+echo "Deployment completed" > /var/www/html/status.txt
